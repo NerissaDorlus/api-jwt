b89f7c19d3dee6cee6ae84d06a0ac4a6889aadd2
Nerissa <nerissadorlus@gmail.com>
2022-06-06 15:33:53 -0400

    initializing JWT and created secret key and put in .gitignore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-const functions = require("firebase-functions");
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const users = [ //mock database
+    {id: 1, email:'john@bocacode.com', password:"abcd123" },
+    {id: 1, email:'jane@bocacode.com',password: 'defg456'},
+    {id: 1, email:'bob@bocacode.com', password: 'ghij789'}
+]
+
+const app= express();
+app.use(cors())
+app.use(express.json());
+
+app.post('/login', (req, res)=> {
+    const{email, password} = req.body
+    //check to see if email and password exist in db
+    //if they do create and send back token
+    //if not send error message
+    const user = users.find(use => user.email === email && use.password === password)
+    if(!user) {
+        res.status(401).send("invalid email or password") 
+        return;  
+     }
+     user.password = undefined //erasing password from user object
+     res.send('Welcome!')
+});
+app.get('/public', (req, res)=> {
+    res.send('Welcome!')
+});
+app.get('/private', (req, res)=> {});
+
+export const api = functions.https.onRequest(app);
