6d004b6a425bc60a9e99d3ff642fdacee731aabd
Nerissa <nerissadorlus@gmail.com>
2022-06-07 14:29:19 -0400

    turned all messages  and errors to json

--- functions/index.js
+++ functions/index.js
@@ -21,34 +21,34 @@ app.post('/login', (req, res)=> {
     //if not send error message
     let user = users.find(user => user.email === email && user.password === password)
     if(!user) {
-        res.status(401).send("invalid email or password") 
+        res.status(401).send({error: "invalid email or password"}) 
         return;  
      }
      user.password = undefined //erasing password from user object
      //create and sign a token
      const token = jwt.sign(user, mySecretKey, {expiresIn: '1hr'}) //.sign creates key and turns it into a token
-     res.send(token)
+     res.send({token})
 });
 
 app.get('/public', (req, res)=> {
-    res.send('Welcome!')
+    res.send({message: 'Welcome!'})
 });
 
 app.get('/private', (req, res)=> {
     //let's require valid token to see this
     const token = req.headers.authorization || "";
     if(!token) {
-        res.status(401).send('You must be logged in to see this')
+        res.status(401).send({error: 'You must be logged in to see this'})
     }
     jwt.verify(token, mySecretKey, (err, decoded) => {
         if(err){
-            res.status(401).send('You must use a valid token to see this');
+            res.status(401).send({error:'You must use a valid token to see this' + error});
             return;
         }
         //we will know if token is valid
         //check if decoded.id is the user they claim to be
-        //authorized to update record in db
-        res.send(`Welcome ${decoded.email}!`)
+        //can update this record in db
+        res.send({message:`Welcome ${decoded.email}!`})
     })
 });
 
